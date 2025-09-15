590dbb239f43bdefa9decde51f793287a0b482a9
weldoy <goshakra@gmail.com>
2025-09-15 17:26:46 +0300

    Семинар + Лекция 1 АиП

--- A&P/Lecture-1.c
+++ A&P/Lecture-1.c
@@ -1,9 +1,17 @@
-// Базовая конструкция "Привет, мир!"
-
 #include <stdio.h>
 
 int main() {
     printf("Hello, World!\n");
     
+    int z = -3, x = 6, d = 3, b = 5;
+    double k, l, m;
+    k = (z / (z + 1));
+    l = ((x - d) / (x + d));
+    m = ((z + b + x) / 3);
+
+    printf("The value k= %f\n", k);
+    printf("The value l= %f\n", l);
+    printf("The value m= %f\n", m);
+
     return 0;
 }
