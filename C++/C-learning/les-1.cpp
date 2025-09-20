efa65c4bc35c41ca4e5c2182ca30ed30fe0b90c3
weldoy <goshakra@gmail.com>
2025-09-20 15:51:32 +0300

    Начало изучения языков C/C++

---
+++ C++/C-learning/les-1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Привет, мир!" << std::endl;
+    return 0;
+}
+
+// Базовая программа на языке C++
+
+// Можно около первой строки подключить 
+// библиотеку using namespace std;
+// и не писать std:: перед cout/endl
